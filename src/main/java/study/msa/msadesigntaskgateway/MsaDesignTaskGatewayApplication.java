ddb4eb6718384c549ebb84dfdeddf3d1925af54f
홍준성 <powerstar13@hanmail.net>
2022-04-22 16:12:32 +0900

    Application > Eureka Client  역할 수행 어노테이션 추가

--- src/main/java/study/msa/msadesigntaskgateway/MsaDesignTaskGatewayApplication.java
+++ src/main/java/study/msa/msadesigntaskgateway/MsaDesignTaskGatewayApplication.java
@@ -2,8 +2,10 @@ package study.msa.msadesigntaskgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient // Eureka Client 역할을 수행한다. YAML에 명시한 유레카 서버에 해당 애플리케이션을 등록한다.
 public class MsaDesignTaskGatewayApplication {
 
     public static void main(String[] args) {
